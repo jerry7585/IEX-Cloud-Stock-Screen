676f6b335a5f9691409d0d6530cd57ed6571fc72
Jerry Su <jerrysu.nyc@gmail.com>
2024-03-17 18:02:30 -0400

    Added secrets.py file and updated .gitignore

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+import numpy as np #numerical computing in python, based in c++ so it is faster
+import pandas as pd #data science library for data manipulation and analysis
+import requests #a popular http library for making http requests, typically through an api, very slow if we can use batch calls it is beter
+import xlsxwriter #creating and manipulating excel files in python
+import math #python mathematical functions
 
+#API Token is in a secrets.py file
 
+print(MY_PUBLISHABLE_API_TOKEN)
