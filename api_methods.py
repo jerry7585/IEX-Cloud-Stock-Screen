e1b7f5b98f22f9f292f9f322c0ce1d564e5e5f5d
Jerry Su <jerrysu.nyc@gmail.com>
2024-03-17 23:00:08 -0400

    Added more columns, more fundamentals, for experienced investors

--- api_methods.py
+++ api_methods.py
@@ -3,8 +3,15 @@ import xlsxwriter #creating and manipulating excel files in python
 
 from my_secrets import IEX_CLOUD_API_TOKEN
 
+
+#base url 
+base_url = 'https://api.iex.cloud/v1/'
+
 #base URL for the IEX Cloud API, global variable, never changes
-base_url = 'https://api.iex.cloud/v1/data/core/quote'
+base_url_quote = 'https://api.iex.cloud/v1/data/core/quote'
+
+#base url for fundamentals
+base_url_fundamentals = 'https://api.iex.cloud/v1/data/core/fundamentals'
 
 #sandbox api for free
 sandbox_base_url = 'https://sandbox.iexapis.com/stable/stock'
@@ -28,29 +35,42 @@ key_parameters = {
     "ytdChange"
 }
 
-dictionary = {}
+quote_dictionary = {}
+fundamentals_dictionary = {}
 
 def fetch_stock_data(ticker):
     
     #endpoint to retrieve the data using the API
-    endpoint = f'{base_url}/{ticker}?token={IEX_CLOUD_API_TOKEN}'
+    quote_endpoint = f'{base_url_quote}/{ticker}?token={IEX_CLOUD_API_TOKEN}'
 
+    #endpoint for fundamentals
+    fundmentals_endpoint = f'{base_url_fundamentals}/{ticker}?token={IEX_CLOUD_API_TOKEN}'
+    
     #sandbox endpoint
     sandbox_endpoint = f'{sandbox_base_url}/{ticker}/quote?token={IEX_CLOUD_API_TOKEN}' 
 
     #send a GET request to the API endpoint
-    response = requests.get(endpoint)
-
+    response = requests.get(quote_endpoint)
+    
     #Check if successful, code is 200 when success
     if response.status_code == 200:
                 
         # Extract the stock quote data from the response JSON
         data = response.json()[0]
-
+        
         #parse it into a dictionary
         for k,v in data.items():
-            dictionary[k] = v
+            quote_dictionary[k] = v
              
     else:
         #Print an error message
         print(f"Failed to fetch stock quote for {ticker}. Status code: ", response.status_code)
+        
+    fundamentals_data = requests.get(fundmentals_endpoint)
+    
+    if fundamentals_data.status_code == 200:
+        for k,v in fundamentals_data.json()[0].items():
+            fundamentals_dictionary[k] = v
+    
+   
+    
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import tkinter as tk
 from tkinter import Text,Scrollbar, simpledialog
 from my_secrets import IEX_CLOUD_API_TOKEN #API Token is in a secrets.py file
 from api_methods import fetch_stock_data #method from other file to perform GET request
-from api_methods import dictionary
+from api_methods import quote_dictionary, fundamentals_dictionary
 
 #make market cap easier to read
 def format_market_cap(market_cap):
@@ -18,8 +18,9 @@ def format_market_cap(market_cap):
     else:
         return market_cap
 
-ticker = "AAPL"
+
 stocks = ['MSFT', 'AAPL', 'NVDA', 'AMZN', 'GOOGL', 'META', 'TSLA', 'LLY', 'V', 'AVGO']
+
 
 my_columns = [
     "Company", 
@@ -29,10 +30,14 @@ my_columns = [
     "PE Ratio", 
     "52 week high", 
     "52 week low", 
-    "YTD Change"
+    "YTD Change",
+    "Revenue",
+    "Revenue Growth",
+    "Profit Margin",
+    "Total Cash Flow",
+    "Long Term Debt"
 ]
 
-#fetch_stock_data(ticker)
 final_dataframe = pd.DataFrame(columns=my_columns)
 pd.set_option('display.max_colwidth', 30)
 
@@ -40,19 +45,26 @@ for stock in stocks:
     fetch_stock_data(stock)
     
     new_data = pd.Series([
-        dictionary.get('companyName'), 
-        dictionary.get('symbol'), 
-        dictionary.get('latestPrice'), 
-        dictionary.get('marketCap'), 
-        dictionary.get('peRatio'), 
-        dictionary.get('week52High'), 
-        dictionary.get('week52Low'), 
-        dictionary.get('ytdChange')
+        quote_dictionary.get('companyName'), 
+        quote_dictionary.get('symbol'), 
+        quote_dictionary.get('latestPrice'), 
+        quote_dictionary.get('marketCap'), 
+        quote_dictionary.get('peRatio'), 
+        quote_dictionary.get('week52High'), 
+        quote_dictionary.get('week52Low'), 
+        quote_dictionary.get('ytdChange'),
+        fundamentals_dictionary.get('revenue'),
+        fundamentals_dictionary.get('pricePerEarningsPerRevenueYoyDeltaPercent'),
+        fundamentals_dictionary.get('profitGrossPerRevenue'),
+        fundamentals_dictionary.get('totalCashFlow'),
+        fundamentals_dictionary.get('liabilitiesNonCurrentDebt')
     ], index=my_columns)
     
     final_dataframe = pd.concat([final_dataframe, new_data.to_frame().T], ignore_index=True)
     
 final_dataframe['Market Cap'] = final_dataframe['Market Cap'].apply(format_market_cap)
+final_dataframe['Revenue'] = final_dataframe['Revenue'].apply(format_market_cap)
+final_dataframe['Total Cash Flow'] = final_dataframe['Total Cash Flow'].apply(format_market_cap)
+final_dataframe['Long Term Debt'] = final_dataframe['Long Term Debt'].apply(format_market_cap)
 
-gui = pg.show(final_dataframe)
-
+gui_quote = pg.show(final_dataframe)
